fff990a67f10247849604f70f958e79d59175df8
Lost Boy <lost.boy.lost.zone@gmail.com>
2025-05-01 09:03:55 +0200

    new user settings pref to enable or disable overlimit max multiplier current and max multiplier basal from original aaps limit

--- core/keys/src/main/kotlin/app/aaps/core/keys/DoubleKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/DoubleKey.kt
@@ -1,5 +1,6 @@
 package app.aaps.core.keys
 import app.aaps.core.keys.BooleanKey.OApsAIMIforcelimits
+
 enum class DoubleKey(
     override val key: String,
     override val defaultValue: Double,
@@ -25,8 +26,20 @@ enum class DoubleKey(
     ApsMaxBasal("openapsma_max_basal", 1.0, 0.1, 25.0, defaultedBySM = true, calculatedBySM = true),
     ApsSmbMaxIob("openapsmb_max_iob", 3.0, 0.0, 70.0, defaultedBySM = true, calculatedBySM = true),
     ApsAmaMaxIob("openapsma_max_iob", 1.5, 0.0, 25.0, defaultedBySM = true, calculatedBySM = true),
-    ApsMaxDailyMultiplier("openapsama_max_daily_safety_multiplier", 3.0, 1.0, 50.0, defaultedBySM = true),
-    ApsMaxCurrentBasalMultiplier("openapsama_current_basal_safety_multiplier", 4.0, 1.0, 50.0, defaultedBySM = true),
+    ApsMaxDailyMultiplier(
+        "openapsama_max_daily_safety_multiplier",
+        3.0,
+        1.0,
+        if (OApsAIMIforcelimits) 50.0 else 10.0, // Inizializzazione diretta
+        defaultedBySM = true
+    ),
+    ApsMaxCurrentBasalMultiplier(
+        "openapsama_current_basal_safety_multiplier",
+        4.0,
+        1.0,
+        if (OApsAIMIforcelimits) 50.0 else 10.0, // Inizializzazione diretta
+        defaultedBySM = true
+    ),
 
     ApsAmaBolusSnoozeDivisor("bolussnooze_dia_divisor", 2.0, 1.0, 10.0, defaultedBySM = true),
     ApsAmaMin5MinCarbsImpact("openapsama_min_5m_carbimpact", 3.0, 1.0, 12.0, defaultedBySM = true),
@@ -49,47 +62,37 @@ enum class DoubleKey(
     ApsAutoIsfSmbDeliveryRatioMax("openapsama_smb_delivery_ratio_max", 0.5, 0.5, 1.0, defaultedBySM = true),
     ApsAutoIsfSmbMaxRangeExtension("openapsama_smb_max_range_extension", 1.0, 1.0, 5.0, defaultedBySM = true),
     EquilMaxBolus("equil_maxbolus", 10.0, 0.1, 25.0),
-    OApsAIMIMaxSMB("key_openapsaimi_max_smb",1.0,0.05,15.0),
-    OApsAIMIHighBGMaxSMB("key_openapsaimi_high_bg_max_smb",1.0,0.05,15.0),
-    OApsAIMIweight("key_aimiweight",50.0,1.0,200.0),
-    OApsAIMICHO("key_cho",50.0,1.0,150.0),
-    OApsAIMITDD7("key_tdd7",40.0,1.0,150.0),
-    OApsAIMIMorningFactor("key_oaps_aimi_morning_factor",50.0,1.0,150.0),
-    OApsAIMIAfternoonFactor("key_oaps_aimi_afternoon_factor",50.0,1.0,150.0),
-    OApsAIMIEveningFactor("key_oaps_aimi_evening_factor",50.0,1.0,150.0),
-    OApsAIMIMealFactor("key_oaps_aimi_meal_factor",50.0,1.0,150.0),
-    OApsAIMIFCLFactor("key_oaps_aimi_FCL_factor",50.0,1.0,150.0),
-    OApsAIMIBFFactor("key_oaps_aimi_BF_factor",50.0,1.0,150.0),
-    OApsAIMIBFPrebolus("key_prebolus_BF_mode",2.5,0.1, 10.0),
-    OApsAIMIBFPrebolus2("key_prebolus2_BF_mode",2.0,0.1, 10.0),
-    OApsAIMILunchFactor("key_oaps_aimi_lunch_factor",50.0,1.0,150.0),
-    OApsAIMIDinnerFactor("key_oaps_aimi_dinner_factor",50.0,1.0,150.0),
-    OApsAIMIHCFactor("key_oaps_aimi_HC_factor",50.0,1.0,150.0),
-    OApsAIMISnackFactor("key_oaps_aimi_snack_factor",50.0,1.0,150.0),
-    OApsAIMIHyperFactor("key_oaps_aimi_hyper_factor",60.0,1.0,150.0),
-    OApsAIMIsleepFactor("key_oaps_aimi_sleep_factor",60.0,1.0,150.0),
-    OApsAIMIMealPrebolus("key_prebolus_meal_mode",2.0,0.1, 10.0),
-    OApsAIMIautodrivePrebolus("key_prebolus_autodrive_mode",1.0,0.1, 10.0),
-    OApsAIMIautodrivesmallPrebolus("key_prebolussmall_autodrive_mode",0.1,0.05, 2.0),
-    OApsAIMIcombinedDelta("key_combinedDelta_autodrive_mode",1.0,0.1, 20.0),
-    OApsAIMIAutodriveDeviation("key_mindeviation_autodrive_mode",1.0,0.1, 5.0),
-    OApsAIMIAutodriveAcceleration("key_Acceleration_autodrive_mode",1.0,0.1, 5.0),
+    OApsAIMIMaxSMB("key_openapsaimi_max_smb", 1.0, 0.05, 15.0),
+    OApsAIMIHighBGMaxSMB("key_openapsaimi_high_bg_max_smb", 1.0, 0.05, 15.0),
+    OApsAIMIweight("key_aimiweight", 50.0, 1.0, 200.0),
+    OApsAIMICHO("key_cho", 50.0, 1.0, 150.0),
+    OApsAIMITDD7("key_tdd7", 40.0, 1.0, 150.0),
+    OApsAIMIMorningFactor("key_oaps_aimi_morning_factor", 50.0, 1.0, 150.0),
+    OApsAIMIAfternoonFactor("key_oaps_aimi_afternoon_factor", 50.0, 1.0, 150.0),
+    OApsAIMIEveningFactor("key_oaps_aimi_evening_factor", 50.0, 1.0, 150.0),
+    OApsAIMIMealFactor("key_oaps_aimi_meal_factor", 50.0, 1.0, 150.0),
+    OApsAIMIFCLFactor("key_oaps_aimi_FCL_factor", 50.0, 1.0, 150.0),
+    OApsAIMIBFFactor("key_oaps_aimi_BF_factor", 50.0, 1.0, 150.0),
+    OApsAIMIBFPrebolus("key_prebolus_BF_mode", 2.5, 0.1, 10.0),
+    OApsAIMIBFPrebolus2("key_prebolus2_BF_mode", 2.0, 0.1, 10.0),
+    OApsAIMILunchFactor("key_oaps_aimi_lunch_factor", 50.0, 1.0, 150.0),
+    OApsAIMIDinnerFactor("key_oaps_aimi_dinner_factor", 50.0, 1.0, 150.0),
+    OApsAIMIHCFactor("key_oaps_aimi_HC_factor", 50.0, 1.0, 150.0),
+    OApsAIMISnackFactor("key_oaps_aimi_snack_factor", 50.0, 1.0, 150.0),
+    OApsAIMIHyperFactor("key_oaps_aimi_hyper_factor", 60.0, 1.0, 150.0),
+    OApsAIMIsleepFactor("key_oaps_aimi_sleep_factor", 60.0, 1.0, 150.0),
+    OApsAIMIMealPrebolus("key_prebolus_meal_mode", 2.0, 0.1, 10.0),
+    OApsAIMIautodrivePrebolus("key_prebolus_autodrive_mode", 1.0, 0.1, 10.0),
+    OApsAIMIautodrivesmallPrebolus("key_prebolussmall_autodrive_mode", 0.1, 0.05, 2.0),
+    OApsAIMIcombinedDelta("key_combinedDelta_autodrive_mode", 1.0, 0.1, 20.0),
+    OApsAIMIAutodriveDeviation("key_mindeviation_autodrive_mode", 1.0, 0.1, 5.0),
+    OApsAIMIAutodriveAcceleration("key_Acceleration_autodrive_mode", 1.0, 0.1, 5.0),
     autodriveMaxBasal("autodrive_max_basal", 1.0, 0.05, 25.0),
     meal_modes_MaxBasal("meal_modes_max_basal", 1.0, 0.05, 25.0),
-    OApsAIMILunchPrebolus("key_prebolus_lunch_mode",2.5,0.1, 10.0),
-    OApsAIMILunchPrebolus2("key_prebolus2_lunch_mode",2.0,0.1, 10.0),
-    OApsAIMIDinnerPrebolus("key_prebolus_dinner_mode",2.5,0.1, 10.0),
-    OApsAIMIDinnerPrebolus2("key_prebolus2_dinner_mode",2.0,0.1, 10.0),
-    OApsAIMISnackPrebolus("key_prebolus_snack_mode",1.0,0.1, 10.0),
-    OApsAIMIHighCarbPrebolus("key_prebolus_highcarb_mode",5.0,0.1, 10.0);
-
-    init {
-        if (OApsAIMIforcelimits) {
-            ApsMaxDailyMultiplier.max = 50.0
-            ApsMaxCurrentBasalMultiplier.max = 50.0
-        } else {
-            ApsMaxDailyMultiplier.max = 10.0
-            ApsMaxCurrentBasalMultiplier.max = 10.0
-        }
-    }
+    OApsAIMILunchPrebolus("key_prebolus_lunch_mode", 2.5, 0.1, 10.0),
+    OApsAIMILunchPrebolus2("key_prebolus2_lunch_mode", 2.0, 0.1, 10.0),
+    OApsAIMIDinnerPrebolus("key_prebolus_dinner_mode", 2.5, 0.1, 10.0),
+    OApsAIMIDinnerPrebolus2("key_prebolus2_dinner_mode", 2.0, 0.1, 10.0),
+    OApsAIMISnackPrebolus("key_prebolus_snack_mode", 1.0, 0.1, 10.0),
+    OApsAIMIHighCarbPrebolus("key_prebolus_highcarb_mode", 5.0, 0.1, 10.0);
 }
