855e9f28389e476f5bf13c4e4685962b7fbb2e8d
mtr93600 <tellier@pm.me>
2025-10-18 21:46:25 +0200

    preparing Dev version

--- core/keys/src/main/kotlin/app/aaps/core/keys/DoubleKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/DoubleKey.kt
@@ -1,5 +1,8 @@
 package app.aaps.core.keys
 
+import app.aaps.core.keys.interfaces.BooleanPreferenceKey
+import app.aaps.core.keys.interfaces.DoublePreferenceKey
+
 enum class DoubleKey(
     override val key: String,
     override val defaultValue: Double,
@@ -12,16 +15,20 @@ enum class DoubleKey(
     override val showInPumpControlMode: Boolean = true,
     override val dependency: BooleanPreferenceKey? = null,
     override val negativeDependency: BooleanPreferenceKey? = null,
-    override val hideParentScreenIfHidden: Boolean = false
+    override val hideParentScreenIfHidden: Boolean = false,
+    override val exportable: Boolean = true
 ) : DoublePreferenceKey {
 
     OverviewInsulinButtonIncrement1("insulin_button_increment_1", 0.5, -5.0, 5.0, defaultedBySM = true, dependency = BooleanKey.OverviewShowInsulinButton),
     OverviewInsulinButtonIncrement2("insulin_button_increment_2", 1.0, -5.0, 5.0, defaultedBySM = true, dependency = BooleanKey.OverviewShowInsulinButton),
     OverviewInsulinButtonIncrement3("insulin_button_increment_3", 2.0, -5.0, 5.0, defaultedBySM = true, dependency = BooleanKey.OverviewShowInsulinButton),
+
     ActionsFillButton1("fill_button1", 0.3, 0.05, 20.0, defaultedBySM = true, hideParentScreenIfHidden = true),
     ActionsFillButton2("fill_button2", 0.0, 0.05, 20.0, defaultedBySM = true),
     ActionsFillButton3("fill_button3", 0.0, 0.05, 20.0, defaultedBySM = true),
+
     SafetyMaxBolus("treatmentssafety_maxbolus", 3.0, 0.1, 60.0),
+
     ApsMaxBasal("openapsma_max_basal", 1.0, 0.1, 25.0, defaultedBySM = true, calculatedBySM = true),
     ApsSmbMaxIob("openapsmb_max_iob", 3.0, 0.0, 70.0, defaultedBySM = true, calculatedBySM = true),
     ApsAmaMaxIob("openapsma_max_iob", 1.5, 0.0, 25.0, defaultedBySM = true, calculatedBySM = true),
@@ -30,10 +37,13 @@ enum class DoubleKey(
     ApsAmaBolusSnoozeDivisor("bolussnooze_dia_divisor", 2.0, 1.0, 10.0, defaultedBySM = true),
     ApsAmaMin5MinCarbsImpact("openapsama_min_5m_carbimpact", 3.0, 1.0, 12.0, defaultedBySM = true),
     ApsSmbMin5MinCarbsImpact("openaps_smb_min_5m_carbimpact", 8.0, 1.0, 12.0, defaultedBySM = true),
+
     AbsorptionCutOff("absorption_cutoff", 6.0, 4.0, 10.0),
     AbsorptionMaxTime("absorption_maxtime", 6.0, 4.0, 10.0),
+
     AutosensMin("autosens_min", 0.7, 0.1, 1.0, defaultedBySM = true, hideParentScreenIfHidden = true),
     AutosensMax("autosens_max", 1.2, 0.5, 3.0, defaultedBySM = true),
+
     ApsAutoIsfMin("autoISF_min", 1.0, 0.3, 1.0, defaultedBySM = true),
     ApsAutoIsfMax("autoISF_max", 1.0, 1.0, 3.0, defaultedBySM = true),
     ApsAutoIsfBgAccelWeight("bgAccel_ISF_weight", 0.0, 0.0, 1.0, defaultedBySM = true),
@@ -47,44 +57,57 @@ enum class DoubleKey(
     ApsAutoIsfSmbDeliveryRatioMin("openapsama_smb_delivery_ratio_min", 0.5, 0.5, 1.0, defaultedBySM = true),
     ApsAutoIsfSmbDeliveryRatioMax("openapsama_smb_delivery_ratio_max", 0.5, 0.5, 1.0, defaultedBySM = true),
     ApsAutoIsfSmbMaxRangeExtension("openapsama_smb_max_range_extension", 1.0, 1.0, 5.0, defaultedBySM = true),
+
+    // === Tes ajouts AIMI / custom ===
     EquilMaxBolus("equil_maxbolus", 10.0, 0.1, 25.0),
+
     OApsAIMIMaxSMB("key_openapsaimi_max_smb", 1.0, 0.05, 15.0),
     OApsAIMIHighBGMaxSMB("key_openapsaimi_high_bg_max_smb", 1.0, 0.05, 15.0),
+
     OApsAIMIweight("key_aimiweight", 50.0, 1.0, 200.0),
     OApsAIMICHO("key_cho", 50.0, 1.0, 150.0),
     OApsAIMITDD7("key_tdd7", 40.0, 1.0, 150.0),
+
     OApsAIMIMorningFactor("key_oaps_aimi_morning_factor", 50.0, 1.0, 150.0),
     OApsAIMIAfternoonFactor("key_oaps_aimi_afternoon_factor", 50.0, 1.0, 150.0),
     OApsAIMIEveningFactor("key_oaps_aimi_evening_factor", 50.0, 1.0, 150.0),
     OApsAIMIMealFactor("key_oaps_aimi_meal_factor", 50.0, 1.0, 150.0),
     OApsAIMIFCLFactor("key_oaps_aimi_FCL_factor", 50.0, 1.0, 150.0),
     OApsAIMIBFFactor("key_oaps_aimi_BF_factor", 50.0, 1.0, 150.0),
+
     OApsAIMIBFPrebolus("key_prebolus_BF_mode", 2.5, 0.1, 10.0),
     OApsAIMIBFPrebolus2("key_prebolus2_BF_mode", 2.0, 0.1, 10.0),
+
     OApsAIMILunchFactor("key_oaps_aimi_lunch_factor", 50.0, 1.0, 150.0),
     OApsAIMIDinnerFactor("key_oaps_aimi_dinner_factor", 50.0, 1.0, 150.0),
     OApsAIMIHCFactor("key_oaps_aimi_HC_factor", 50.0, 1.0, 150.0),
     OApsAIMISnackFactor("key_oaps_aimi_snack_factor", 50.0, 1.0, 150.0),
     OApsAIMIHyperFactor("key_oaps_aimi_hyper_factor", 60.0, 1.0, 150.0),
     OApsAIMIsleepFactor("key_oaps_aimi_sleep_factor", 60.0, 1.0, 150.0),
+
     OApsAIMIMealPrebolus("key_prebolus_meal_mode", 2.0, 0.1, 10.0),
     OApsAIMIautodrivePrebolus("key_prebolus_autodrive_mode", 1.0, 0.1, 10.0),
     OApsAIMIautodrivesmallPrebolus("key_prebolussmall_autodrive_mode", 0.1, 0.05, 2.0),
+
     OApsAIMIcombinedDelta("key_combinedDelta_autodrive_mode", 1.0, 0.1, 20.0),
     OApsAIMIAutodriveDeviation("key_mindeviation_autodrive_mode", 1.0, 0.1, 5.0),
     OApsAIMIAutodriveAcceleration("key_Acceleration_autodrive_mode", 1.0, 0.1, 5.0),
+
     autodriveMaxBasal("autodrive_max_basal", 1.0, 0.05, 25.0),
     meal_modes_MaxBasal("meal_modes_max_basal", 1.0, 0.05, 25.0),
+
     OApsAIMILunchPrebolus("key_prebolus_lunch_mode", 2.5, 0.1, 10.0),
     OApsAIMILunchPrebolus2("key_prebolus2_lunch_mode", 2.0, 0.1, 10.0),
     OApsAIMIDinnerPrebolus("key_prebolus_dinner_mode", 2.5, 0.1, 10.0),
     OApsAIMIDinnerPrebolus2("key_prebolus2_dinner_mode", 2.0, 0.1, 10.0),
     OApsAIMISnackPrebolus("key_prebolus_snack_mode", 1.0, 0.1, 10.0),
     OApsAIMIHighCarbPrebolus("key_prebolus_highcarb_mode", 5.0, 0.1, 10.0),
+
     OApsAIMIwcycledateday("key_wcycledateday", 1.0, 1.0, 31.0),
     OApsAIMIwcyclemenstruation("key_wcyclemenstruation", 10.0, 1.0, 30.0),
     OApsAIMIwcycleovulation("key_wcycleovulation", 5.0, 1.0, 30.0),
     OApsAIMIwcycleluteal("key_wcycleluteal", 15.0, 1.0, 30.0),
+
     OApsAIMINightGrowthMinRiseSlope("key_oaps_aimi_ngr_min_rise_slope", 5.0, 0.5, 30.0),
     OApsAIMINightGrowthSmbMultiplier("key_oaps_aimi_ngr_smb_multiplier", 1.2, 1.0, 1.5),
     OApsAIMINightGrowthBasalMultiplier("key_oaps_aimi_ngr_basal_multiplier", 1.1, 1.0, 1.5),
