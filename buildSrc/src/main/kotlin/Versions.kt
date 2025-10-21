69a0e1e3216c5c6097b95cb4ffcf6d46e9f24079
mtr93600 <tellier@pm.me>
2025-10-21 21:01:37 +0200

    merge Milos + update yml and Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.3.3.0-dev-c.AIMI.181025"
+    const val appVersion = "3.3.3.0-dev-c.AIMI.211025"
     const val versionCode = 1500
 
     const val compileSdk = 36
